95c357aa2e0999e45d1f2fe86a4d641ba02a1801
Judith Jude <ifunanyajudith09@gmail.com>
2025-01-30 21:35:24 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h1>Ip address tracker</h1>
+    <div className="">
+      <h1 className=" card">Ip address tracker</h1>
     </div>
   );
 };
